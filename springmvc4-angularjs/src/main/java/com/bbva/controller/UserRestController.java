500d2783a5c7099ba9a66ec1246cd7834992610c
José Díaz <jose.diaz@joedayz.pe>
2015-11-26 15:43:50 -0500

    Edit user

--- springmvc4-angularjs/src/main/java/com/bbva/controller/UserRestController.java
+++ springmvc4-angularjs/src/main/java/com/bbva/controller/UserRestController.java
@@ -21,5 +21,17 @@ public class UserRestController {
         return new ResponseEntity<List<User>>(users, HttpStatus.OK);
     }
  
- 
+     
+    //-------------------Retrieve Single User--------------------------------------------------------
+     
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> getUser(@PathVariable("id") long id) {
+        System.out.println("Fetching User with id " + id);
+        User user = userService.findById(id);
+        if (user == null) {
+            System.out.println("User with id " + id + " not found");
+            return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
  }
