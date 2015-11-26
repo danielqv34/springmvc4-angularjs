926809d62f2432c0637bbe22b4511e81faaed49f
Jose Diaz <jose.diaz@joedayz.pe>
2015-11-26 12:42:22 -0500

    configurado para web

--- springmvc4-angularjs/src/main/java/com/bbva/controller/IndexController.java
+++ springmvc4-angularjs/src/main/java/com/bbva/controller/IndexController.java
@@ -2,13 +2,11 @@ package com.bbva.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/")
 public class IndexController {
 
-	  @RequestMapping(method = RequestMethod.GET)
+		@RequestMapping("/")
 	    public String getIndexPage() {
 	        return "UserManagement";
 	    }
