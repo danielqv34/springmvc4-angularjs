f57e50857228f1483168e43b48a2805b6be313d2
José Díaz <jose.diaz@joedayz.pe>
2015-11-26 16:09:16 -0500

    UserController.js

---
+++ springmvc4-angularjs/src/main/resources/static/js/controller/user_controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+App.controller('UserController', ['$scope', 'UserService', function($scope, UserService) {
+          var self = this;
+          
+      }]);          
+          
+          
