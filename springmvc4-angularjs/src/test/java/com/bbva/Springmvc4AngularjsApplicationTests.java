ff0fd4c80c18ff35f297bb683aeb5877afd97a17
Jose Diaz <jose.diaz@joedayz.pe>
2015-11-26 11:42:11 -0500

    Primera version

---
+++ springmvc4-angularjs/src/test/java/com/bbva/Springmvc4AngularjsApplicationTests.java
@@ -0,0 +1,18 @@
+package com.bbva;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Springmvc4AngularjsApplication.class)
+@WebAppConfiguration
+public class Springmvc4AngularjsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
