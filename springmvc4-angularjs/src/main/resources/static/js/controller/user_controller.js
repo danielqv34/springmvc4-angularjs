e1c460c0b06ddac2bcc4a956b2376be9fd587a0d
José Díaz <jose.diaz@joedayz.pe>
2015-11-26 16:13:37 -0500

    Obtener usuarios

--- springmvc4-angularjs/src/main/resources/static/js/controller/user_controller.js
+++ springmvc4-angularjs/src/main/resources/static/js/controller/user_controller.js
@@ -2,7 +2,20 @@
 
 App.controller('UserController', ['$scope', 'UserService', function($scope, UserService) {
           var self = this;
-          
+          self.user={id:null,username:'',address:'',email:''};
+          self.users=[];
+              
+          self.fetchAllUsers = function(){
+              UserService.fetchAllUsers()
+                  .then(
+      					       function(d) {
+      						        self.users = d;
+      					       },
+            					function(errResponse){
+            						console.error('Error while fetching Currencies');
+            					}
+      			       );
+          };
       }]);          
           
           
