15729aaf807230721559312908389b4cef2ed91f
Jose Diaz <jose.diaz@joedayz.pe>
2015-11-26 11:50:34 -0500

    Configuracion de application

--- springmvc4-angularjs/src/main/java/com/bbva/Springmvc4AngularjsApplication.java
+++ springmvc4-angularjs/src/main/java/com/bbva/Springmvc4AngularjsApplication.java
@@ -2,10 +2,20 @@ package com.bbva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Springmvc4AngularjsApplication {
+public class Springmvc4AngularjsApplication 
+	extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder 
+	configure(SpringApplicationBuilder application) {
+		return application.sources(Springmvc4AngularjsApplication.class);
+	}
+	
+	
     public static void main(String[] args) {
         SpringApplication.run(Springmvc4AngularjsApplication.class, args);
     }
