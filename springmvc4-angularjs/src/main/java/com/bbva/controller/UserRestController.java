e2ecf5ffe414b638c240748687e7f6208cf366be
José Díaz <jose.diaz@joedayz.pe>
2015-11-26 15:41:57 -0500

    UserRestController
    Primera versión . no compila

---
+++ springmvc4-angularjs/src/main/java/com/bbva/controller/UserRestController.java
@@ -0,0 +1,25 @@
+package com.bbva.controller;
+
+import java.util.List;
+
+//resolver imports
+
+RestController
+public class UserRestController {
+ 
+    @Autowired
+    UserService userService; 
+    
+        //-------------------Retrieve All Users--------------------------------------------------------
+     
+    @RequestMapping(value = "/user/", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> listAllUsers() {
+        List<User> users = userService.findAllUsers();
+        if(users.isEmpty()){
+            return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+        }
+        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+    }
+ 
+ 
+ }
